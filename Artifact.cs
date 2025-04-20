fc585248226c66f6c356f900486d7508773973a3
Nastya <nasteagaydarly@gmail.com>
2025-04-20 11:37:20 +0300

    add artifact entity, add method overloading, as/is operators

---
+++ Artifact.cs
@@ -0,0 +1,47 @@
+﻿namespace Assignment_2
+{
+    public class Artifact
+    {
+        public const int MinRequiredLevel = 5;
+
+        public string Name { get; init; }
+        public int PowerBoost { get; set; }
+        public Artifact(string name, int powerBoost = 10)
+        {
+            Name = name;
+            PowerBoost = powerBoost;
+        }
+            
+        public void ApplyTo(Сharacter target)
+        {
+            if (target.Level < MinRequiredLevel)
+            {
+                Console.WriteLine($"{target.Name} is not experienced enough to use {Name}");
+                return;
+            }
+
+            if (target is Soldier soldier)
+            {
+                soldier.Strength += PowerBoost;
+                Console.WriteLine($"{Name} has increased {soldier.Name}'s strength by {PowerBoost}");
+                return;
+            }
+
+            else if (target is Mage mage)
+            {
+                mage.Mana += PowerBoost;
+                Console.WriteLine($"{Name} has boosted {mage.Name}'s mana by {PowerBoost * 2}");
+            }
+
+            else
+                Console.WriteLine($"{Name} has no effect on {target.Name}.");
+        }
+
+        public void ApplyTo(Сharacter target, int bonusBoost)
+        {
+            ApplyTo(target);
+            Console.WriteLine($"{Name} gave an additional bonus of {bonusBoost} to {target.Name}.");
+            target.Heal(bonusBoost);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿Сharacter mage = new Mage("Magecik1", 3, 100, 50, 10);
-Сharacter soldier = new Soldier("Soldiercik1", 12, 120, 15);
+﻿using Assignment_2;
+
+Сharacter mage = new Mage("Magecik1", 5, 100, 50, 10);
 Сharacter mage2 = new Mage("Magecik2", 10, 0, 50, 10);
+
+Сharacter soldier = new Soldier("Soldiercik1", 12, 120, 15);
 Сharacter soldier2 = new Soldier("Soldiercik2", 12, 0, 15);
 
 Console.WriteLine("Before Attack:");
@@ -22,9 +25,16 @@ team.AddCharacter(mage2);
 team.AddCharacter(soldier);
 team.AddCharacter(soldier2);
 
-Console.WriteLine("\nListik:");
+var artifact = new Artifact("Artifactik", 15);
 
 foreach (var character in team)
 {
-    Console.WriteLine($"{character.Name} - Health: {character.Health}");
+    var magishka = character as Mage;
+    if (magishka is not null)
+        artifact.ApplyTo(magishka);
+    else
+        Console.WriteLine($"{character.Name} is not a Mage, artifact cannot be applied.");
 }
+
+artifact.ApplyTo(soldier); 
+artifact.ApplyTo(target: mage, bonusBoost: 5); 
